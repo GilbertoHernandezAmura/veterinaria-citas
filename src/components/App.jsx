212d678f4c8ddb0a28b6a39fc409be4aabd8167f
Gilberto Hernandez Amura <gilbertohernandezamura@gmail.com>
2023-06-12 23:15:39 -0300

    Tailwindcss installed and some components added

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import ListadoPacientes from './ListadoPacientes.jsx';
+import Formulario from './Formulario.jsx';
+import Header from './Header.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
+    </div>
+  );
+};
+
+export default App;
