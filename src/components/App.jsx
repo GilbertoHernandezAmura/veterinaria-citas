8b5e87110cd5fa0b6dcee9a35fdabcab8d4c2f0b
Gilberto Hernandez Amura <gilbertohernandezamura@gmail.com>
2023-06-19 01:41:25 -0300

    Added localStorage to the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ListadoPacientes from './ListadoPacientes.jsx';
 import Formulario from './Formulario.jsx';
 import Header from './Header.jsx';
@@ -6,6 +6,25 @@ import Header from './Header.jsx';
 const App = () => {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  // With [] (empty dependencies) the code inside will execute only once (initial render)
+  // Recuperando pacientes de LS
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+
+      if (Array.isArray(pacientesLS) && pacientesLS.length) {
+        setPacientes(pacientesLS);
+      }
+    };
+
+    obtenerLS();
+  }, []);
+
+  // Enviando pacientes al LS
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
 
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter((p) => p.id !== id);
